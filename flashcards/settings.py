d985a75bc38131ba5b7f556784a1de58d9f2eae0
Your Name <you@example.com>
2025-03-09 04:42:58 +0100

    Added ALLOWED_HOSTS

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flashcards-dw0y.onrender.com']
 
 
 # Application definition
